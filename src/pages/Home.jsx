54b0a28fa45181fc3e3641b509e6a71d2ea7486d
isha152002 <ishajain152002@gmail.com>
2026-01-18 12:48:02 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+export const Home = () =>{
+    return (
+    <div className="min-h-screen">
+
+    {/*theme toggle*/}
+
+    {/*background effect*/}
+
+    {/*nav bar */}
+
+    {/*main content */}
+
+    {/*footer */}
+
+    </div>
+    )
+}
