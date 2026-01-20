acd210f7590da97a3e26a9223a23a581c6720066
isha152002 <ishajain152002@gmail.com>
2026-01-21 00:40:19 +0530

    theme button adjust

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,7 +53,7 @@ export const Navbar = () => {
 
          <button 
           onClick={()=> setIsMenuOpen((prev) => !prev)}
-          className="md:hidden p-2 text-foreground z-50"
+          className="md:hidden fixed top-4 right-15 p-2 text-foreground z-50"
           aria-label={isMenuOpen? "Close Menu": "Open Menu"}
          >
             {isMenuOpen? <X size={24}/> : <Menu size={24}/>}{" "}
