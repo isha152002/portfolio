a44d8cc08eb8ae587b362942328ef3a475b3eb2f
isha152002 <ishajain152002@gmail.com>
2026-01-20 21:52:00 +0530

    skill section created

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -25,8 +25,10 @@ export const HeroSection= () => {
         </div>
 
         <div className="absolute bottom-8 left-1/2 transform-transalate-x-1/2 flex flex-col items-center animate-bounce">
+        <a href="#about">
             <span className="text-sm text-muted-foreground mb-2 text-glow"> Scroll </span>
             <ArrowDown className="h-5 w-5 text-primary "/>
+            </a>
         </div>
     </section>
     )
--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,3 +1,30 @@
+
+const skills=[
+    "HTML",
+    "CSS",
+    "React",
+    "C",
+    "Tailwind CSS",
+    "C++",
+    "JavaScript",
+    //add more later
+]
+
 export const SkillsSection = () =>{
-    return <section></section>
+    return (<section id="skills">
+        <div className="mt-20 animate-fade-in animation-delay-600">
+            <h2 className="text-3xl md:text-4xl font-bold mb-1 text-center">
+                    My<span className="text-primary"> Skills</span>
+                </h2>
+            <div className="relative overflow-hidden">
+                <div className="flex animate-slide">
+                    {[...skills,...skills,...skills].map((skill,idx)=>(
+                        <div key={idx} className="flex-shrink-0 px-8 py-4">
+                            <span className="text-xl font-semibold text-muted-foreground/50 hover:text-muted-foreground transition-colors">{skill}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    </section>)
 }
